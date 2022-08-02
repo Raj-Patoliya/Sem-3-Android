46de3aeef940c496511e6a79f68b7af85a579a17
Raj Patoliya <84698326+Raj-Patoliya@users.noreply.github.com>
2022-08-02 11:42:08 +0530

    Create Q1.kt

---
+++ Q1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val a:Int = 10;
+    val b:Int= 2;
+    var c = 0;
+    c = a+b;
+   	print("$a + $b  =  $c");
+    c = a-b;
+   	print("$a - $b  =  $c");
+    c = a*b;
+   	print("$a * $b  =  $c");
+    c = a/b;
+   	print("$a / $b  =  $c");
+}
