d36837b0b1cf4ee2c8c6b69310a224e008de7f38
Raj Patoliya <84698326+Raj-Patoliya@users.noreply.github.com>
2022-08-02 12:04:44 +0530

    Create Q3.kt

---
+++ Q3.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+fun main() {  
+    println("Enter Number : ")  
+    var input: Int =Integer.valueOf(readLine())  
+     if(input%2== 0)
+    {    
+        println("$input is even Number");
+    }
+    else
+    {
+        println("$input is Odd Number")
+    }
+}  
