714388c3c1b2d2a969e83faa3e76241bd02bdff8
Raj Patoliya <84698326+Raj-Patoliya@users.noreply.github.com>
2022-08-02 11:42:46 +0530

    Update Q1.kt

--- Q1.kt
+++ Q1.kt
@@ -3,11 +3,11 @@ fun main() {
     val b:Int= 2;
     var c = 0;
     c = a+b;
-   	print("$a + $b  =  $c");
+   	print("\n\t$a + $b  =  $c");
     c = a-b;
-   	print("$a - $b  =  $c");
+   	print("\n\t$a - $b  =  $c");
     c = a*b;
-   	print("$a * $b  =  $c");
+   	print("\n\t$a * $b  =  $c");
     c = a/b;
-   	print("$a / $b  =  $c");
+   	print("\n\t$a / $b  =  $c");
 }
